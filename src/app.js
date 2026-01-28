6f9e82ed4e8bce519e2692901e50247663b8948d
vaghela karmrajsinh <kvaghela.it@gmail.com>
2026-01-28 21:38:36 +0530

    add allowed methods to CORS configuration

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH","DELETE"],
 }))
 
 app.use(express.json({ limit: "16kb" }))
