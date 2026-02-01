174c277aa04d3d2736648ce22837d466ad64b750
vaghela karmrajsinh <kvaghela.it@gmail.com>
2026-02-01 17:16:23 +0530

    add updateUserAvatar controller to handle avatar updates

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -314,6 +314,51 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
 
 })
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(
+            400,
+            "Avatar file is missing "
+        )
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(
+            400,
+            "Error while uploading avtar file"
+        )
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res
+        .status(200)
+        .json(
+            200,
+            user,
+            "Avtar file is Updated Successfully"
+        )
+
+
+})
+
+
+
 export {
     registerUser,
     loginUser,
@@ -321,5 +366,6 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar
 }
