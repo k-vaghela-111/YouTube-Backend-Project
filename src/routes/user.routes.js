02c421bd680eb6d633ea6cfb568e99e52a6b0077
vaghela karmrajsinh <kvaghela.it@gmail.com>
2026-02-04 17:19:37 +0530

    add routes for controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -21,5 +21,15 @@ router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-user-details").post(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").post(verifyJWT, updateUserAvatar)
+
+router.route("/update-coverimage").post(verifyJWT, updateUserCoverImage)
+
+router.route("/channel-profile").get(getUserChannelProfile)
 
 export default router
